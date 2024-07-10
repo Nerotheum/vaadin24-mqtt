b0b8ac2da1503f68c7f77741e4ed6d582f8f0426
Nerotheum <Nerotheum@gmail.com>
2024-07-10 20:07:19 +0200

    Display status of mqtt connection on MainView

--- src/main/java/com/nerotheum/vaadinmqtt/MainView.java
+++ src/main/java/com/nerotheum/vaadinmqtt/MainView.java
@@ -2,11 +2,13 @@ package com.nerotheum.vaadinmqtt;
 
 import java.util.List;
 
+import com.nerotheum.vaadinmqtt.mqtt.MqttConnectionService;
 import com.nerotheum.vaadinmqtt.mqtt.MqttMessage;
 import com.nerotheum.vaadinmqtt.mqtt.MqttMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -17,17 +19,24 @@ public class MainView extends VerticalLayout {
 
     private final MqttMessageService mqttMessageService;
     private final Grid<MqttMessage> mqttMessageGrid;
+    private final MqttConnectionService mqttConnectionService;
 
-    public MainView(@Autowired MqttMessageService service) {
-        this.mqttMessageService = service;
+    public MainView(@Autowired MqttMessageService mqttMessageService, @Autowired MqttConnectionService mqttConnectionService) {
+        this.mqttMessageService = mqttMessageService;
         this.mqttMessageGrid = new Grid<>(MqttMessage.class);
-        add(mqttMessageGrid);
+        this.mqttConnectionService = mqttConnectionService;
     }
 
     @PostConstruct
     private void init() {
+        String mqttConnection = mqttConnectionService.getMqttClient().isConnected() ? "success" : "error";
+        Span mqttStatus = new Span("MQTT connection status: " + mqttConnection);
+        mqttStatus.getElement().getThemeList().add("badge " + mqttConnection);
+        add(mqttStatus);
+
         List<MqttMessage> messages = mqttMessageService.findAll();
         mqttMessageGrid.setItems(messages);
         mqttMessageGrid.setColumns("id", "topic", "message", "dateTime");
+        add(mqttMessageGrid);
     }
 }
